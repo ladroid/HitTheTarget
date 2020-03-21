2d9cf962d345456053523bbd197e698061d22e00
ladroid <puatachok@gmail.com>
2020-03-21 18:18:06 +0200

    bug fixes

--- script/SpaceWorld.gd
+++ script/SpaceWorld.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 #export (int) var attack_range = 5
 export (int) var speed = 10000
+var enemies = preload("res://scenes/Enemy.tscn")
+
+func _ready():
+	var screen_size = get_viewport().get_visible_rect().size
+	for i in range(0,10):
+		var enemy = enemies.instance()
+		randomize()
+		var x = rand_range(0,screen_size.x)
+		var y = rand_range(0,screen_size.y)
+		enemy.position.y = y
+		enemy.position.x = x
+		add_child(enemy)
+
 func _physics_process(delta):
-	#var player = scn_player.instance()
 	var dir = ($SpaceShip.global_position - $Enemy.global_position).normalized()
-	#move_and_collide(dir * speed * delta)
 	$Enemy.move_and_slide(dir * speed * delta)
-	
-	#if $Enemy.get_slide_count() > 0:
-	#	for i in range($Enemy.get_slide_count()):
-	#		print("BNBN")
-	#		if "Laser" in $Enemy.get_slide_collision(i).collider.name:
-	#			print("AAA")
-	#			$Enemy.get_slide_collision(i).collider.dead()
 
 
 func _on_SpaceShip_shoot(bullet, pos, dir):
